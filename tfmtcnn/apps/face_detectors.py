4f41d0b14e5cf53dad91edcc4138b4acc58b1266
kosarev@smedx.com <kosarev@smedx.com>
2019-05-19 09:26:01 +0300

    flag to switch off/on showing images has been added

--- tfmtcnn/apps/face_detectors.py
+++ tfmtcnn/apps/face_detectors.py
@@ -15,7 +15,8 @@ outdir = tfmtcnn.dirname().joinpath(os.pardir, 'output')
 
 
 @click.command()
-@click.option('--detector', default='tfmtcnn', help='type of face detector, tfmtcnn or pypi-mtcnn')
+@click.option('--detector', default='tfmtcnn', help='type of face detector, tfmtcnn or pypimtcnn')
+@click.option('--show', default=True, help='show face detections')
 def main(**args):
     detector = FaceDetector(detector=args['detector'])
 
@@ -23,18 +24,23 @@ def main(**args):
 
     for image, path in loader:
         boxes = detector.detect(image)
+        print('\nimage', path, image.shape)
+        print('number of detected faces', len(boxes))
+        print(boxes)
 
-        # show rectangles
-        for bbox in boxes:
-            position = (int(bbox[0]), int(bbox[1]))
-            cv2.putText(image, str(np.round(bbox[4], 2)), position, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 255))
-            cv2.rectangle(image, position, (int(bbox[2]), int(bbox[3])), (0, 0, 255))
-
+        # save images
         ioutils.write_image(image, path.name, prefix=outdir)
 
-        cv2.imshow(str(path), image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # show rectangles
+        if args['show']:
+            for bbox in boxes:
+                position = (int(bbox[0]), int(bbox[1]))
+                cv2.putText(image, str(np.round(bbox[4], 2)), position, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 255))
+                cv2.rectangle(image, position, (int(bbox[2]), int(bbox[3])), (0, 0, 255))
+
+            cv2.imshow(str(path), image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
